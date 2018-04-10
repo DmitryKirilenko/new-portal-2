13748e01871cad92a6d6c0a914dbf6b0fb8e02c9
DmitryKirilenko <kirildim16@gmail.com>
2018-04-10 21:18:27 +0300

    go

--- main-menu.js
+++ main-menu.js
@@ -1,3 +1,13 @@
   document.addEventListener("DOMContentLoaded", function(event) {
-    alert("DOM fully loaded and parsed");
+    let buttonNews=document.getElementById('dropdown');
+    let hide=document.querySelector('.hidden');
+    buttonNews.onclick=function(){
+        hide.className='show';
+    }
+    var body=document.getElementsByTagName('body');
+    body[0].onclick=function(event){
+      if(event.target.classList[0]=="content"){
+         drop.className='hide'; 
+      }
+    }
   });
