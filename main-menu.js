e8e74ac2f3c966d44ec809da5d8d7ab7ac71edc5
dmitry <kirildim16@gmail.com>
2018-04-10 22:36:32 +0300

    menu ready

--- main-menu.js
+++ main-menu.js
@@ -1,13 +1,15 @@
-  document.addEventListener("DOMContentLoaded", function(event) {
-    let buttonNews=document.getElementById('dropdown');
-    let hide=document.querySelector('.hidden');
-    buttonNews.onclick=function(){
-        hide.className='show';
-    }
-    var body=document.getElementsByTagName('body');
-    body[0].onclick=function(event){
-      if(event.target.classList[0]=="content"){
-         drop.className='hide'; 
-      }
-    }
-  });
+document.addEventListener('DOMContentLoaded', function () {
+      let drop = document.querySelector('#dropdown');
+      let hide = document.querySelector('.hide');
+      let body = document.getElementsByTagName('body')
+      drop.addEventListener('click',function(){
+      	   hide.className = 'show';
+      })
+
+     body[0].addEventListener('click',function(event){
+      	    if(event.target.classList[0] == 'content'){
+      	    	hide.className = 'hide';
+      	    }
+
+      })
+     });
